e998066d2da175c50135108c01501617824e32db
Patrick Krämer <patagonahn@gmail.com>
2022-12-07 03:30:01 +0100

    feat: add demo project

---
+++ src/AdbClient.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace AdbClient.Demo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Process.Start("adb", "start-server");
+
+            var client = new AdbServicesClient();
+            var cts = new CancellationTokenSource(60000);
+            try
+            {
+                await foreach (var item in client.TrackDevices(cts.Token))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+    }
+}
